4aea4ebc1b5b555e4476d4a9ca276cb70451de6f
Pikalugia <75184384+Pikalugia@users.noreply.github.com>
2023-03-08 12:45:54 +0100

    Update and rename githubIcon.vue to peekioIcon.vue

---
+++ src/components/layout/peekioIcon.vue
@@ -0,0 +1,17 @@
+<template>
+
+<div id="PeekioIcon">
+
+  <a target="_blank" href="https://peekio.no/">
+    <img :src="require('../../assets/img/peekio.png')">
+  </a>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "PeekioIcon"
+}
+</script>
